7671c9d09a3ffa96b9d68c1472c74ea5f946ea91
bruno edoh <edohbruno@gmail.com>
2021-04-26 00:02:46 +0000

    zustand api structure

--- src/vanilla.js
+++ src/vanilla.js
@@ -1,1 +1,21 @@
-const create = () => {}
+const create = () => {
+  let state
+
+  const listeners = new Set()
+
+  const setState = () => {}
+
+  const getState = () => state
+
+  const subscribeWithSelector = () => {}
+
+  const subscribe = () => {}
+
+  const destroy = () => listeners.clear()
+
+  const api = { setState, getState, subscribe, destroy }
+
+  // state = createState()
+
+  return api
+}
